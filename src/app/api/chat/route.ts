49b896c85ee784699992742fe407408fcc837014
lormanzo3 <unframed3@gmail.com>
2026-09-18 10:03:40 +0900

    added API key and now building API function

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY,
+});
+
+export async function POST(request: Request) {
+    const { message } = await request.json();
+    const response = await anthropic.messages.create({
+        model: "claude-haiku-4-5-20251001",
+        max_tokens: 1024,
+        messages: [
+            { reole: "user", content: message },
+        ],
+    })
+}
